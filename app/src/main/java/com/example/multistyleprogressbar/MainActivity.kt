4aeb1a3b69365f1c4ab6835799c4874b0418530c
xushuai <xushuai@cozyla.com>
2025-11-14 15:31:35 +0800

    init

---
+++ app/src/main/java/com/example/multistyleprogressbar/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.multistyleprogressbar
+
+import android.os.Bundle
+import android.os.Handler
+import android.os.Message
+import android.widget.Button
+import android.widget.ProgressBar
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity(), Handler.Callback {
+    private lateinit var handler: Handler
+    var progressbar: ProgressBar? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        handler = Handler(mainLooper, this)
+        progressbar = findViewById<ProgressBar>(R.id.progressBar)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            handler.sendEmptyMessageDelayed(0, 1)
+
+        }
+    }
+
+    override fun handleMessage(msg: Message): Boolean {
+        progressbar?.progress = progressbar?.progress?.plus(1)!!
+        if (progressbar?.progress != 100)
+            handler.sendEmptyMessageDelayed(0, 1)
+        return true
+    }
+}
